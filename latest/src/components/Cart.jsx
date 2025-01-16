62c2feab07ea41f4420fabe619f2487575945cce
raunakjr <raunakmishra9798@gmail.com>
2025-01-17 02:14:25 +0530

    modified css

--- latest/src/components/Cart.jsx
+++ latest/src/components/Cart.jsx
@@ -5,7 +5,12 @@ export default function Cart() {
   const { cartData, Removedata } = useProduct();
 
   let data = cartData;
-
+  const truncateText = (text, limit) => {
+    if (text.length > limit) {
+      return text.substring(0, limit) + "...";
+    }
+    return text;
+  };
   return data && data.length > 0 ? (
     <div className="product-container">
       {data.map((e, idx) => {
@@ -13,7 +18,9 @@ export default function Cart() {
           <div className="product-card" key={idx}>
             <img className="product-image" src={e.image} alt={e.title} />
             <p className="product-category">Category: {e.category}</p>
-            <p className="product-description">Description: {e.description}</p>
+            <p className="product-description">
+              Description: {truncateText(e.description, 100)}
+            </p>
             <p className="product-price">Price: ${e.price}</p>
             <button onClick={() => Removedata(e.id)}> Remove From Cart</button>
           </div>
