bafa18b18c92944344d7ef0b23984dfe1cc1be44
raunakjr <raunakmishra9798@gmail.com>
2025-01-17 16:50:26 +0530

    with context api

--- latest/src/components/Navbar.jsx
+++ latest/src/components/Navbar.jsx
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 import { useProduct } from "../contexts/product.context";
 
 export default function Navbar() {
+  const { mode, setMode } = useProduct();
   const { cartData } = useProduct();
+  const handleclick = () => {
+    console.log("clicked");
+    if (mode == "Light") setMode("Dark");
+    else setMode("Light");
+  };
   return (
     <nav className="navbar">
       <h1>ShopEasy</h1>
@@ -11,6 +17,7 @@ export default function Navbar() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/cart">Bag {cartData.length} </Link>
+        <button onClick={() => handleclick()}>Dark</button>
       </div>
     </nav>
   );
--- latest/src/contexts/product.context.jsx
+++ latest/src/contexts/product.context.jsx
@@ -7,29 +7,53 @@ const ProductProvider = ({ children }) => {
     return JSON.parse(localStorage.getItem("key1")) || [];
   });
 
+  const [mode, setMode] = useState(() => {
+    return JSON.parse(localStorage.getItem("key2")) || "Light";
+  });
+
+  useEffect(() => {
+    const container = document.querySelector(".product-container");
+    if (mode === "Dark") {
+      if (container) container.style.backgroundColor = "black";
+    } else {
+      if (container) container.style.backgroundColor = "white";
+    }
+  }, []);
+
   useEffect(() => {
     localStorage.setItem("key1", JSON.stringify(cartData));
   }, [cartData]);
 
+  useEffect(() => {
+    localStorage.setItem("key2", JSON.stringify(mode));
+    const container = document.querySelector(".product-container");
+    if (mode === "Dark") {
+      if (container) container.style.backgroundColor = "black";
+    } else {
+      if (container) container.style.backgroundColor = "white";
+    }
+  }, [mode]);
+
   const Adddata = (e) => {
-    const checkalready = cartData.some((d) => d == e);
+    const checkalready = cartData.some((d) => d === e);
     if (checkalready) return;
     setCartdata([...cartData, e]);
   };
 
   const Removedata = (id) => {
-    const newArr = cartData.filter((e) => e.id != id);
+    const newArr = cartData.filter((e) => e.id !== id);
     setCartdata(newArr);
   };
 
   return (
     <Productcontext.Provider
-      value={{ cartData, setCartdata, Adddata, Removedata }}
+      value={{ cartData, setCartdata, Adddata, Removedata, setMode, mode }}
     >
       {children}
     </Productcontext.Provider>
   );
 };
+
 export default ProductProvider;
 export const useProduct = () => {
   return useContext(Productcontext);
